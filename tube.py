6b9d1c886202ba044b58274897ec47b959c6b23f
Zern <75311133+BG47510@users.noreply.github.com>
2023-10-02 19:13:51 +0200

    Add files via upload

---
+++ tube.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+from urllib.parse import unquote
+import requests
+import re
+import sys
+
+windows = False
+if 'win' in sys.platform:
+    windows = True
+
+erreur = 'https://raw.githubusercontent.com/BG47510/Zap/main/assets/error.m3u8'
+
+
+def snif(url):
+    lien = s.get(url, timeout=15).text
+    retour = re.findall(r'\"hlsManifestUrl\":\"(.*?)\"\}', lien)
+    tri = unquote(''.join(retour))
+    flux = requests.get(tri).text
+    if '.m3u8' not in tri:
+        print(erreur)
+    else:
+        print(flux)
+
+s = requests.Session()
+result = snif(str(sys.argv[1]))
+print(result)
