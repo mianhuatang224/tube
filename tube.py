c2e627a63d8038ff576b62215f5d63cbea1359c2
Zern <75311133+BG47510@users.noreply.github.com>
2024-01-20 19:34:49 +0100

    Update tube.py

--- tube.py
+++ tube.py
@@ -13,11 +13,11 @@ def snif(url):
     retour = re.findall(r'\"hlsManifestUrl\":\"(.*?)\"\}', lien)
     tri = unquote(''.join(retour))
     flux = requests.get(tri).text
-    print(flux)
-#    if '#EXTM3U' not in tri:
-   #     print(erreur)
-#    else:
-   #     print(flux)
+   # print(flux)
+    if '#.m3u8' not in tri:
+        print(erreur)
+    else:
+        print(flux)
 
 s = requests.Session()
 result = snif(str(sys.argv[1]))
